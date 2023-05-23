232249f9c363f3ecfd9fc5bfe703a70f68860d1a
Kent Wu <kent0113@163.com>
2023-05-24 00:58:06 +0800

    remove some useless ui elements

--- modules/ui.py
+++ modules/ui.py
@@ -1741,14 +1741,14 @@ def create_ui():
 
     with gr.Blocks(theme=shared.gradio_theme, analytics_enabled=False, title="GRAVITI Diffus") as demo:
         with gr.Row(elem_id="topbar"):
-            with gr.Column(elem_id="user-setting", min_width=500, scale=2):
-                gr.HTML(
-                    value="<div class='user-content'><div class='right-content'><div class='discord-icon'><a title='Join Discord' href='https://discord.gg/QfBbBYqQ7z'><img src='/public/image/discord.png' /></a></div><div title='Unlock more credits' class='upgrade-content' style='display: none'><a href='/user#/subscription?type=subscription'><img src='/public/image/lightning.png'/><span>Upgrade</span></a></div></div>"
-                            "<div style='display: none;justify-content: flex-end;' class='user_info'><a href='/user'><img "
-                            "src=''"
-                            "/></a><div class='user_info-name'><span></span><a "
-                            "href='/api/logout' target='_self'>Log out</a></div></div></div>",
-                    show_label=False)
+            # with gr.Column(elem_id="user-setting", min_width=500, scale=2):
+            #     gr.HTML(
+            #         value="<div class='user-content'><div class='right-content'><div class='discord-icon'><a title='Join Discord' href='https://discord.gg/QfBbBYqQ7z'><img src='/public/image/discord.png' /></a></div><div title='Unlock more credits' class='upgrade-content' style='display: none'><a href='/user#/subscription?type=subscription'><img src='/public/image/lightning.png'/><span>Upgrade</span></a></div></div>"
+            #                 "<div style='display: none;justify-content: flex-end;' class='user_info'><a href='/user'><img "
+            #                 "src=''"
+            #                 "/></a><div class='user_info-name'><span></span><a "
+            #                 "href='/api/logout' target='_self'>Log out</a></div></div></div>",
+            #         show_label=False)
             with gr.Column(scale=6, min_width=850):
                 with gr.Row(elem_id="quicksettings"):
                     for i, k, item in sorted(quicksettings_list, key=lambda x: quicksettings_names.get(x[1], x[0])):
--- modules/ui_extra_networks.py
+++ modules/ui_extra_networks.py
@@ -419,17 +419,17 @@ def create_ui(container, button, tabname):
                 tab.select(fn=None, _js=f"modelTabClick", inputs=[tab_click_params], outputs=[])
                 ui.pages.append(page_elem)
                 with gr.Row(elem_id=f"{ui.tabname}_{self_name_id}_pagination", elem_classes="pagination"):
-                     with gr.Column(scale=7):
-                         gr.Button("hide", visible=False)
-                     with gr.Column(elem_id=f"{ui.tabname}_{self_name_id}_upload_btn", elem_classes="pagination_upload_btn", scale=2,  min_width=220):
-                        upload_btn = gr.Button(f"Upload {page.title} Model", variant="primary")
-                        upload_btn.click(
-                            fn=None,
-                            _js=f'''() => {{
-                                if (typeof register_button == "undefined") {{document.querySelector("#{upload_button_id}").click();}}
-                                else {{document.querySelector("#{button_id}").click();}}
-                            }}'''
-                        )
+                     # with gr.Column(scale=7):
+                     #     gr.Button("hide", visible=False)
+                     # with gr.Column(elem_id=f"{ui.tabname}_{self_name_id}_upload_btn", elem_classes="pagination_upload_btn", scale=2,  min_width=220):
+                     #    # upload_btn = gr.Button(f"Upload {page.title} Model", variant="primary")
+                     #    upload_btn.click(
+                     #        fn=None,
+                     #        _js=f'''() => {{
+                     #            if (typeof register_button == "undefined") {{document.querySelector("#{upload_button_id}").click();}}
+                     #            else {{document.querySelector("#{button_id}").click();}}
+                     #        }}'''
+                     #    )
                      with gr.Column(elem_id=f"{ui.tabname}_{self_name_id}_pagination_row", elem_classes="pagination_row",  min_width=220):
                         gr.HTML(
                             value="<div class='pageniation-info'>"
