6b50f79068376731483ca1a8c10cc9d732cb705d
Kent Wu <kent0113@163.com>
2023-05-26 00:48:38 +0800

    start with dark theme

--- relauncher-awe.py
+++ relauncher-awe.py
@@ -19,7 +19,8 @@ while n < 5:
 
     launch_string = f"./webui.sh -f --port {args.port} --device-id {args.device_id} --opt-sdp-attention --listen " \
                     f"--logging-file-dir ./logs --logging-level DEBUG " \
-                    f"--system-monitor-addr http://47.100.10.24:8080/api/monitor --system-monitor-api-secret API_SECRET"
+                    f"--system-monitor-addr http://47.100.10.24:8080/api/monitor --system-monitor-api-secret API_SECRET " \
+                    f"--theme dark"
 
     print(f"[Device {args.device_id}] - launch_string: {launch_string}")
     os.system(launch_string)
