5123f132fd6c692132303d531da93c2e508aebc0
Kent Wu <kent0113@163.com>
2023-05-27 00:03:17 +0800

    add start args

--- relauncher-awe.py
+++ relauncher-awe.py
@@ -20,7 +20,7 @@ while n < 5:
     launch_string = f"./webui.sh -f --port {args.port} --device-id {args.device_id} --opt-sdp-attention --listen " \
                     f"--logging-file-dir ./logs --logging-level DEBUG " \
                     f"--system-monitor-addr http://47.100.10.24:8080/api/monitor --system-monitor-api-secret API_SECRET " \
-                    f"--theme dark"
+                    f"--theme dark --opt-channelslast"
 
     print(f"[Device {args.device_id}] - launch_string: {launch_string}")
     os.system(launch_string)
