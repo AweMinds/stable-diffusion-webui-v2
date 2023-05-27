ded58f42d48443b76a133ccdc62518f284ab3d62
Kent Wu <kent0113@163.com>
2023-05-27 23:14:48 +0800

    add log

--- relauncher-awe.py
+++ relauncher-awe.py
@@ -1,29 +1,34 @@
 import argparse
 import os
 import time
+import logging
+
+logging.basicConfig(filename='./logs/start.log',
+                    level=logging.DEBUG,
+                    format='%(asctime)s [%(levelname)s] (%(name)s:%(lineno)d): %(message)s')
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-p", "--port", help="端口")
 parser.add_argument("-d", "--device-id", help="GPU的序号")
 args = parser.parse_args()
 
-print("port: " + args.port)
-print("device-id: " + args.device_id)
+logging.info("port: " + args.port)
+logging.info("device-id: " + args.device_id)
 
 n = 0
 while True:
-    print(f"[Device {args.device_id}] -  Relauncher: Launching...")
+    logging.info(f"[Device {args.device_id}] -  Relauncher: Launching...")
 
     if n > 0:
-        print(f'\tRelaunch count: {n}')
+        logging.info(f'\tRelaunch count: {n}')
 
     launch_string = f"./webui.sh -f --port {args.port} --device-id {args.device_id} --opt-sdp-attention --listen " \
                     f"--logging-file-dir ./logs --logging-level DEBUG " \
                     f"--theme dark --opt-channelslast"
     # f"--system-monitor-addr http://47.100.10.24:8080/api/monitor --system-monitor-api-secret API_SECRET " \
 
-    print(f"[Device {args.device_id}] - launch_string: {launch_string}")
+    logging.info(f"[Device {args.device_id}] - launch_string: {launch_string}")
     os.system(launch_string)
-    print(f"[Device {args.device_id}] - Relauncher: Process is ending. Relaunching in 2s...")
+    logging.info(f"[Device {args.device_id}] - Relauncher: Process is ending. Relaunching in 2s...")
     n += 1
     time.sleep(2)
