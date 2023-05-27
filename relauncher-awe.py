2c97d0a0c2cf039f666d0f8f51ca5daa2270fe7e
Kent Wu <kent0113@163.com>
2023-05-27 22:40:06 +0800

    remove monitor and run never stop

--- relauncher-awe.py
+++ relauncher-awe.py
@@ -11,7 +11,7 @@ print("port: " + args.port)
 print("device-id: " + args.device_id)
 
 n = 0
-while n < 5:
+while True:
     print(f"[Device {args.device_id}] -  Relauncher: Launching...")
 
     if n > 0:
@@ -19,8 +19,8 @@ while n < 5:
 
     launch_string = f"./webui.sh -f --port {args.port} --device-id {args.device_id} --opt-sdp-attention --listen " \
                     f"--logging-file-dir ./logs --logging-level DEBUG " \
-                    f"--system-monitor-addr http://47.100.10.24:8080/api/monitor --system-monitor-api-secret API_SECRET " \
                     f"--theme dark --opt-channelslast"
+    # f"--system-monitor-addr http://47.100.10.24:8080/api/monitor --system-monitor-api-secret API_SECRET " \
 
     print(f"[Device {args.device_id}] - launch_string: {launch_string}")
     os.system(launch_string)
