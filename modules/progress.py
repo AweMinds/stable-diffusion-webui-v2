2fc218c9beab61b2bf74683602086f1e971fc348
Kent Wu <kent0113@163.com>
2023-06-12 23:24:57 +0800

    revert shared.state.time_start

--- modules/progress.py
+++ modules/progress.py
@@ -161,7 +161,7 @@ def progressapi(req: ProgressRequest):
 
     progress = min(progress, 1)
 
-    elapsed_since_start = time.time() - 0 if not shared.state.time_start else shared.state.time_start
+    elapsed_since_start = time.time() - shared.state.time_start
     predicted_duration = elapsed_since_start / progress if progress > 0 else None
     eta = predicted_duration - elapsed_since_start if predicted_duration is not None else None
 
